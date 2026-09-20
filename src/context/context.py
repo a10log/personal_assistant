dc994a49320e35c4986560b407694e11da84e590
Cugerbol <cugerbol@student.21-school.ru>
2026-09-20 10:13:23 +0300

    rename resources to context

---
+++ src/context/context.py
@@ -0,0 +1,24 @@
+import os
+from dataclasses import dataclass
+
+from pydantic import Field
+from langchain_gigachat.chat_models import GigaChat
+from src.context.prompt_provider import SystemPromptProvider
+
+
+@dataclass
+class Context:
+	llm: GigaChat = Field(description="Базовый ЛЛМ провайдер")
+	prompts: SystemPromptProvider = Field(description="Подгружает промпты")
+
+def get_context():
+	return Context(
+		prompts=SystemPromptProvider(),
+		llm=GigaChat(
+			credentials=os.environ["GIGACHAT_CREDENTIALS"],
+			scope=os.environ["GIGACHAT_SCOPE"],
+			model=os.environ["GIGACHAT_MODEL"],
+			verify_ssl_certs=False,
+			timeout=120,
+		)
+	)
--- src/nodes/call_llm.py
+++ src/nodes/call_llm.py
@@ -1,10 +1,10 @@
 from langgraph.graph import MessagesState
 from langgraph.runtime import Runtime
-from src.resources.resources import Resources
+from src.context.context import Context
 from langchain_core.messages import SystemMessage, AIMessage
 
 
-def call_llm(state: MessagesState, runtime: Runtime[Resources]):
+def call_llm(state: MessagesState, runtime: Runtime[Context]):
 
 	system_prompt: str = runtime.context.prompts["main_system_prompt"]
 	system_msg: SystemMessage = SystemMessage(content=system_prompt)
